4cd26f39c6f3f1fa63d333a21a58d035943fb7fa
76DCHT22209-39-Bathuhang <bathuhang07@gmail.com>
2026-08-18 12:22:06 +0700

    BT01-CP1: Tao cau truc

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+struct nhanvien{
+    string mannv;
+    string tennv;
+    float luongcb;
+    float heso;
+};
+
+int main(){
+    int n;
+    nhanvien ds[100];
+
+    return 0;
+}
